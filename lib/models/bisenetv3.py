e566b7648229e5c433cd59f95fefd5250973d4bb
sandyfish1989 <woshigaohan@163.com>
2023-11-13 19:02:11 +0800

    Update bisenetv3.py

--- lib/models/bisenetv3.py
+++ lib/models/bisenetv3.py
@@ -1,20 +1,21 @@
-from .utils import *
+from utils import *
+
 
 class BiSeNetV3(nn.Module):
-    def __init__(self, num_classes, aux_mode='train'):
+    def __init__(self, n_classes, aux_mode='train'):
         super(BiSeNetV3, self).__init__()
         self.aux_mode = aux_mode
         self.detail = DetailBranch()
         self.segment = SegmentBranch()
 
         self.bga = SimBGABlock(128, 128)
-        #aux1 aux2 辅助训练
-        self.head = SegmentHead(128, 512, num_classes, up_factor=8)
-        self.aux1 = SegmentHead(128, 128, num_classes, up_factor=8)
-        self.aux2 = SegmentHead(64, 128, num_classes, up_factor=16)
+        # aux1 aux2 辅助训练
+        self.head = SegmentHead(128, 512, n_classes, up_factor=8)
+        self.aux1 = SegmentHead(128, 128, n_classes, up_factor=8)
+        self.aux2 = SegmentHead(64, 128, n_classes, up_factor=16)
 
     def forward(self, x):
-        detail = self.detail(x) # [bs, 128, 80, 80]
+        detail = self.detail(x)  # [bs, 128, 80, 80]
         feat8, feat16 = self.segment(x)
         feat_head = self.bga(detail, feat8)
         out = self.head(feat_head)
@@ -25,12 +26,13 @@ class BiSeNetV3(nn.Module):
             return out, out_aux1, out_aux2
         return out
 
-def bisenetv3(num_classes):
-    model = BiSeNetV3(num_classes)
+
+if __name__ == "__main__":
+    model = BiSeNetV3(n_classes=19)
 
     import copy
-    import thop #获取计算参量
+    import thop  # 获取计算参量
+
     model_tmp = copy.deepcopy(model)
-    flops, params = thop.profile(model_tmp, inputs=(torch.randn(1, 3, 640, 640), ))
-    print("%.2fG" % (flops/1e9), "%.2fM" % (params/1e6))
-    return model
+    flops, params = thop.profile(model_tmp, inputs=(torch.randn(1, 3, 640, 640),))
+    print("%.2fG" % (flops / 1e9), "%.2fM" % (params / 1e6))
